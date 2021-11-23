db3115f401ccff9a86972fa757890ef1811add45
MuadDev <muaddev@gmail.com>
2021-11-23 14:07:11 +0100

    Complete type hinting

--- parallel/function.py
+++ parallel/function.py
@@ -3,9 +3,9 @@
 from multiprocessing import Pool
 
 from tqdm import tqdm
-from typing import Any, List
+from typing import Any, Callable, List
 
-def execute_function_in_parallel(f, arguments: List[Any], verbose: bool = True) -> List[Any]:
+def execute_function_in_parallel(f: Callable, arguments: List[Any], verbose: bool = True) -> List[Any]:
     """Executes function *f* in parallel passing in each item in *arguments* as a single argument
 
     From: https://stackoverflow.com/a/45276885/8488985
